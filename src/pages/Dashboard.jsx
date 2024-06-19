e32e5d5291099b4c8808b1fca5220d69336f6018
Akshyat Pathak <114989068+mysojinyeast@users.noreply.github.com>
2024-06-19 22:24:23 +0530

    Dashboard completed with Search feature added to it

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -12,13 +12,13 @@ function DashboardPage() {
         setSearch(newValue);
     };
 
-    // Filter coins based on search input
+    
     const filteredCoins = coins.filter((item) =>
         item.name.toLowerCase().includes(search.toLowerCase())|| item.symbol.toLowerCase().includes(search.toLowerCase())
     );
 
     useEffect(() => {
-        // Get 100 Coins
+        
         axios
             .get(
                 "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
