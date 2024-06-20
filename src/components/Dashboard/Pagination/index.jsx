f04641ddf20e25d0f68a21ff9de1f663a87d6e82
Akshyat Pathak <114989068+mysojinyeast@users.noreply.github.com>
2024-06-20 19:48:49 +0530

    Pagination added on Dashboard

---
+++ src/components/Dashboard/Pagination/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./styles.css";
+import Pagination from "@mui/material/Pagination";
+
+export default function PaginationControlled({ page, handlePageChange }) {
+  return (
+    <div className="pagination-div">
+      <Pagination
+        sx={{
+          "& .MuiPaginationItem-text": {
+            color: "#fff !important",
+            border: "1px solid var(--grey)",
+          },
+          "& .MuiPaginationItem-text:hover": {
+            backgroundColor: "transparent !important",
+          },
+          "& .Mui-selected  ": {
+            backgroundColor: "var(--blue)",
+            borderColor: "var(--blue)",
+          },
+          "& .MuiPaginationItem-ellipsis": {
+            border: "none",
+          },
+        }}
+        count={10} // Assuming you have 10 pages for the pagination
+        page={page}
+        onChange={handlePageChange}
+      />
+    </div>
+  );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,22 +3,23 @@ import axios from 'axios';
 import Header from '../components/Common/Header';
 import TabsComponent from '../components/Dashboard/Tabcomponent';
 import Search from '../components/Dashboard/Search';
+import PaginationControlled from '../components/Dashboard/Pagination';
 
 function DashboardPage() {
     const [coins, setCoins] = useState([]);
     const [search, setSearch] = useState("");
+    const [paginatedCoins, setPaginatedCoins] = useState([]);
+    const [page, setPage] = useState(1);
 
     const onSearchChange = (newValue) => {
         setSearch(newValue);
     };
 
-    
     const filteredCoins = coins.filter((item) =>
-        item.name.toLowerCase().includes(search.toLowerCase())|| item.symbol.toLowerCase().includes(search.toLowerCase())
+        item.name.toLowerCase().includes(search.toLowerCase()) || item.symbol.toLowerCase().includes(search.toLowerCase())
     );
 
     useEffect(() => {
-        
         axios
             .get(
                 "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
@@ -26,17 +27,32 @@ function DashboardPage() {
             .then((response) => {
                 console.log("RESPONSE>>>", response.data);
                 setCoins(response.data);
+                setPaginatedCoins(response.data.slice(0, 10));
             })
             .catch((error) => {
                 console.log("ERROR>>>", error.message);
             });
     }, []);
 
+    const handlePageChange = (event, value) => {
+        setPage(value);
+        var initialCount = (value - 1) * 10;
+        setPaginatedCoins(filteredCoins.slice(initialCount, initialCount + 10));
+    };
+
     return (
         <div>
             <Header />
             <Search search={search} onSearchChange={onSearchChange} />
-            <TabsComponent coins={filteredCoins} />
+            <TabsComponent
+                coins={search ? filteredCoins : paginatedCoins}
+            />
+            {!search && (
+                <PaginationControlled
+                    page={page}
+                    handlePageChange={handlePageChange}
+                />
+            )}
         </div>
     );
 }
